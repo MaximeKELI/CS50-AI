f35d8f3fd4e07be019fa1d6fce1d106955371902
MaximeKELI <presidentnetero01@gmail.com>
2025-07-08 13:55:01 +0000

    CS50AI

--- search/degrees/degrees/degrees.py
+++ search/degrees/degrees/degrees.py
@@ -1,7 +1,12 @@
+"""
+A program that determines how many "degrees of sepatraion" apart
+two hollywood actors are. Each degree consists of a film that two actors both starred in
+"""
+
 import csv
 import sys
 
-from util import Node, StackFrontier, QueueFrontier
+from util import Node, QueueFrontier
 
 # Maps names to a set of corresponding person_ids
 names = {}
@@ -18,8 +23,8 @@ def load_data(directory):
     Load data from CSV files into memory.
     """
     # Load people
-    with open(f"{directory}/people.csv", encoding="utf-8") as f:
-        reader = csv.DictReader(f)
+    with open(f"{directory}/people.csv", encoding="utf-8") as file:
+        reader = csv.DictReader(file)
         for row in reader:
             people[row["id"]] = {
                 "name": row["name"],
@@ -32,8 +37,8 @@ def load_data(directory):
                 names[row["name"].lower()].add(row["id"])
 
     # Load movies
-    with open(f"{directory}/movies.csv", encoding="utf-8") as f:
-        reader = csv.DictReader(f)
+    with open(f"{directory}/movies.csv", encoding="utf-8") as file:
+        reader = csv.DictReader(file)
         for row in reader:
             movies[row["id"]] = {
                 "title": row["title"],
@@ -42,8 +47,8 @@ def load_data(directory):
             }
 
     # Load stars
-    with open(f"{directory}/stars.csv", encoding="utf-8") as f:
-        reader = csv.DictReader(f)
+    with open(f"{directory}/stars.csv", encoding="utf-8") as file:
+        reader = csv.DictReader(file)
         for row in reader:
             try:
                 people[row["person_id"]]["movies"].add(row["movie_id"])
@@ -53,6 +58,11 @@ def load_data(directory):
 
 
 def main():
+    """ Main Function
+    Take arguments from command line and get which dataset the AI
+    will be using. Then use the shortest path function to get the
+    degrees of separation between two stars
+    """
     if len(sys.argv) > 2:
         sys.exit("Usage: python degrees.py [directory]")
     directory = sys.argv[1] if len(sys.argv) == 2 else "large"
@@ -69,19 +79,23 @@ def main():
     if target is None:
         sys.exit("Person not found.")
 
-    path = shortest_path(source, target)
+    if source == target:
+        print("You have chosen the same person!")
 
-    if path is None:
-        print("Not connected.")
     else:
-        degrees = len(path)
-        print(f"{degrees} degrees of separation.")
-        path = [(None, source)] + path
-        for i in range(degrees):
-            person1 = people[path[i][1]]["name"]
-            person2 = people[path[i + 1][1]]["name"]
-            movie = movies[path[i + 1][0]]["title"]
-            print(f"{i + 1}: {person1} and {person2} starred in {movie}")
+        path = shortest_path(source, target)
+
+        if path is None:
+            print("Not connected.")
+        else:
+            degrees = len(path)
+            print(f"{degrees} degrees of separation.")
+            path = [(None, source)] + path
+            for i in range(degrees):
+                person1 = people[path[i][1]]["name"]
+                person2 = people[path[i + 1][1]]["name"]
+                movie = movies[path[i + 1][0]]["title"]
+                print(f"{i + 1}: {person1} and {person2} starred in {movie}")
 
 
 def shortest_path(source, target):
@@ -92,9 +106,41 @@ def shortest_path(source, target):
     If no possible path, returns None.
     """
 
-    # TODO
-    raise NotImplementedError
+    # BFS search guarantee shortest path
+    frontier = QueueFrontier()
+    frontier.add(Node(source, None, None))
 
+    searched = []
+    searched.append(source)
+
+    while not frontier.empty():
+        # Get the next node and remove it from frontier
+        cur_node = frontier.remove()
+        if cur_node.state == target:
+            path = back_track(cur_node)
+            return path
+        for (movie_id, person_id) in neighbors_for_person(cur_node.state):
+            if not person_id in searched:
+                searched.append(person_id)
+                node = Node(person_id, cur_node, movie_id)
+                frontier.add(node)
+
+    return None
+
+def back_track(node):
+    """
+    Back track from a node to the origin node through
+    each node's parent node
+    """
+    path = []
+    cur_node = node
+    while cur_node.parent is not None:
+        movie_id = cur_node.action
+        person_id = cur_node.state
+        path.insert(0, (movie_id, person_id))
+        cur_node = cur_node.parent
+
+    return path
 
 def person_id_for_name(name):
     """
@@ -130,8 +176,8 @@ def neighbors_for_person(person_id):
     movie_ids = people[person_id]["movies"]
     neighbors = set()
     for movie_id in movie_ids:
-        for person_id in movies[movie_id]["stars"]:
-            neighbors.add((movie_id, person_id))
+        for person in movies[movie_id]["stars"]:
+            neighbors.add((movie_id, person))
     return neighbors
 
 
